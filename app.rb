1ff577a634b468aacd94a3098193d4264e4350d1
Mutsa <mutsa.evelyn@hotmail.com>
2017-07-17 17:29:28 +0100

    Completed Introducing Params: Added name and first name params using instance variables

--- app.rb
+++ app.rb
@@ -10,7 +10,14 @@ get '/secret' do
   'Hello secret world!'
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = %w(Amigo Oscar Viking).sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
+  @last_name = params[:last_name]
   erb(:index)
 end
